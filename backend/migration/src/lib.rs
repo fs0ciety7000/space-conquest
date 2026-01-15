58d8c2a79893f78a038181cf206917a2db8f38fa
phant0mhex <nd.managment@gmail.com>
2026-01-15 23:36:07 +0100

    update migr

--- backend/migration/src/lib.rs
+++ backend/migration/src/lib.rs
@@ -21,7 +21,6 @@ mod m20260111_222219_create_fleet_mission;
 mod m20260111_223746_create_transport_log;
 mod m20260112_004006_create_message_table;
 mod m20260112_032740_add_solar_plant;
-mod m20260112_043825_add_shipyard_level;
 mod m20260112_054113_add_hangar_level;
 mod m20260112_060815_create_construction_queue;
 mod m20260112_072815_add_usernames_to_logs;
@@ -54,7 +53,6 @@ impl MigratorTrait for Migrator {
             Box::new(m20260111_223746_create_transport_log::Migration),
             Box::new(m20260112_004006_create_message_table::Migration),
             Box::new(m20260112_032740_add_solar_plant::Migration),
-            Box::new(m20260112_043825_add_shipyard_level::Migration),
             Box::new(m20260112_054113_add_hangar_level::Migration),
             Box::new(m20260112_060815_create_construction_queue::Migration),
             Box::new(m20260112_072815_add_usernames_to_logs::Migration),
